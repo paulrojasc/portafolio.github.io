3ea35c5d7c48be4e4ae5acbfe545fc9635aab30b
Paul Rojas <paulrojasc@gmail.com>
2023-06-08 15:23:22 -0400

    Se agrega home, projects, contact, CDNS y Cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+  get "/home", to: "pages#home"
+  get "/projects", to: "pages#projects"
+  get "/contact", to: "pages#contact"
 end
